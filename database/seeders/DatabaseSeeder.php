40b661aed05cab27427b83a052945b418afbded3
Dabeey <abbasdaughter18@gmail.com>
2025-11-09 07:35:49 +0100

    skip duplicate if admin exists

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\Property;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,17 +13,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Create a random user
         User::factory(1)->create();
 
-        User::factory()->create([
-            'name' => 'Admin',
-            'email' => 'admin@realestate.com',
-            'password' => bcrypt('admin123')
-        ]);
+        // Create admin only if it doesn't already exist
+        if (!User::where('email', 'admin@realestate.com')->exists()) {
+            User::factory()->create([
+                'name' => 'Admin',
+                'email' => 'admin@realestate.com',
+                'password' => bcrypt('admin123'),
+            ]);
+        }
 
+        // Create properties
         Property::factory(70)->create();
     }
 }
-
-
-
